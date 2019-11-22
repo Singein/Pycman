69fb3559a0baf4c642297d0952a2cb13b348434c
singein <singein@outlook.com>
2019-11-22 17:06:34 +0800

    build(package.py): removed version, changed commit script

--- package.py
+++ package.py
@@ -1,12 +1,11 @@
 package_info = {
     'name': 'Pycman',
-    'version': '0.0.3',
     'author': 'singein',
     'email': 'singein@outlook.com'
 }
 
 scripts = {
-    'commit': 'cz c',
+    'commit': 'git add . && cz commit',
     'build': 'python setup.py bdist_wheel',
     'default': 'echo 请输入明确的命令名称'
 }
