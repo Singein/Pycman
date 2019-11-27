1310f142053f032227de8a7a66b7adf8188c84cf
singein <singein@outlook.com>
2019-11-27 14:30:03 +0800

    [release] 0.1.10

--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 package = {
     'name': 'Pycman',
-    'version': '0.1.9',
+    'version': '0.1.10',
     'author': 'singein',
     'email': 'singein@outlook.com',
     "scripts": {
--- pycman/__init__.py
+++ pycman/__init__.py
@@ -1,6 +1,6 @@
 import sys
 import os
 
-__version__ = '0.1.9'
+__version__ = '0.1.10'
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
